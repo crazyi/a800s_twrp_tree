1b01e3b6b8c02b58eda68e086ee0070f40cc299f
crazyi <waal334@163.com>
2013-01-12 23:13:31 +0800

    for jellybean

--- cm.mk
+++ cm.mk
@@ -1,18 +1,13 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := ef39s
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/pantech/ef39s/device_ef39s.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef39s
-PRODUCT_NAME := full_ef39s
-PRODUCT_BRAND := Pantech
-PRODUCT_MODEL := SKY IM-A800S
-PRODUCT_MANUFACTURER := Pantech
+PRODUCT_NAME := cm_ef39s
+PRODUCT_BRAND := pantech
+PRODUCT_MODEL := ef39s
+PRODUCT_MANUFACTURER := pantech
--- runatboot.sh
+++ runatboot.sh
@@ -5,8 +5,8 @@
 sleep 3
 busybox dd if=/dev/zero bs=512 seek=264 count=1 conv=notrunc of=/dev/block/mmcblk0p10
 # correct the time
-# sleep 2
-# a=`cat /sdcard/time/time`
-# chmod 0777 /sdcard/time/busybox
-# b=`/sdcard/time/busybox date +%s`
-# /sdcard/time/busybox date -s `/sdcard/time/busybox date +%Y%m%d%H%M.%S -D '%s' -d $(($a+$b))`
+sleep 2
+a=`cat /sdcard/time/time`
+chmod 0777 /sdcard/time/busybox
+b=`/sdcard/time/busybox date +%s`
+/sdcard/time/busybox date -s `/sdcard/time/busybox date +%Y%m%d%H%M.%S -D '%s' -d $(($a+$b))`
